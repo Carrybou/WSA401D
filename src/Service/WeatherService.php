194d56ed9b3b5f3d541f067bb3bbbbdd485c8041
Carrybou <thomasguibert94@gmail.com>
2024-05-15 13:49:02 +0200

    Appel api temperature actuelle, prévision sur 5j et enregistrement des données dans un fichiers pendant 5min

---
+++ src/Service/WeatherService.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Cocur\Slugify\Slugify;
+
+
+class WeatherService
+{
+    private $httpClient;
+    private $apiKey;
+    private $cacheDir;
+    private $slugify;
+
+
+    public function __construct(HttpClientInterface $httpClient, string $apiKey, string $projectDir)
+    {
+        $this->httpClient = $httpClient;
+        $this->apiKey = $apiKey;
+        $this->cacheDir = $projectDir . '/public/data';
+        $this->slugify = new Slugify();
+    }
+
+    public function getWeatherData(string $city): array
+    {
+        $citySlug = $this->slugify->slugify($city);
+        $cacheFile = $this->cacheDir . '/' . $citySlug . '.json';
+
+        if (file_exists($cacheFile) && (filemtime($cacheFile) > (time() - 5 * 60))) {
+            $data = json_decode(file_get_contents($cacheFile), true);
+        } else {
+            if (file_exists($cacheFile)) {
+                unlink($cacheFile);
+            }
+            $response = $this->httpClient->request('GET', "https://api.openweathermap.org/data/2.5/weather", [
+                'query' => [
+                    'q' => $city,
+                    'units' => 'metric',
+                    'appid' => $this->apiKey,
+                ]
+            ]);
+
+            $data = $response->toArray();
+
+            file_put_contents($cacheFile, json_encode($data));
+        }
+
+        return $data;
+    }
+    public function getForecastData(float $lat, float $lon,string $city): array
+    {
+        $citySlug = $this->slugify->slugify($city);
+        $cacheFile = $this->cacheDir . '/' . $citySlug . '_forecast.json';
+
+        if (file_exists($cacheFile) && (filemtime($cacheFile) > (time() - 5 * 60))) {
+            $data = json_decode(file_get_contents($cacheFile), true);
+        } else {
+            if (file_exists($cacheFile)) {
+                unlink($cacheFile);
+            }
+            $response = $this->httpClient->request('GET', "https://api.openweathermap.org/data/2.5/forecast", [
+                'query' => [
+                    'lat' => $lat,
+                    'lon' => $lon,
+                    'units' => 'metric',
+                    'appid' => $this->apiKey,
+                ]
+            ]);
+
+            $data = $response->toArray();
+
+            file_put_contents($cacheFile, json_encode($data));
+        }
+
+        return $data;
+    }
+}
