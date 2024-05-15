13b7634f2a0838881a01aa06cb75ad2757e0f398
Carrybou <thomasguibert94@gmail.com>
2024-05-15 16:18:46 +0200

    graph add

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -6,6 +6,7 @@ use App\Service\WeatherService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpClient\HttpClient;
 
 class WeatherController extends AbstractController
 {
@@ -19,7 +20,7 @@ class WeatherController extends AbstractController
     #[Route('/', name: 'weather_home')]
     public function index(): Response
     {
-        $city = 'London';
+        $city = 'Paris';
         $weatherData = $this->weatherService->getWeatherData($city);
         $temperature = $weatherData['main']['temp'];
 
@@ -28,10 +29,25 @@ class WeatherController extends AbstractController
         $forecastData = $this->weatherService->getForecastData($lat, $lon,$city);
         $forecast = $forecastData['list'];
 
+        $now = time();
+        $next24Hours = $now + 24 * 60 * 60;
+
+        $forecastNext24Hours = array_filter($forecast, function ($entry) use ($now, $next24Hours) {
+            $forecastTime = strtotime($entry['dt_txt']);
+            return $forecastTime >= $now && $forecastTime <= $next24Hours;
+        });
+
+        $chartData = [
+            'labels' => array_map(function ($entry) { return $entry['dt_txt']; }, $forecastNext24Hours),
+            'temperature' => array_map(function ($entry) { return $entry['main']['temp']; }, $forecastNext24Hours),
+            'windSpeed' => array_map(function ($entry) { return $entry['wind']['speed']; }, $forecastNext24Hours)
+        ];
+
         return $this->render('weather/index.html.twig', [
             'city' => $city,
             'temperature' => $temperature,
             'forecast' => $forecast,
+            'chartData' => $chartData
         ]);
     }
 }
